b64b8bc2f5cc6ad1c030fef998e9d18e03a6497a
Meï-Garino Jérémy <jeremy@c-g-m.org>
2014-09-11 10:01:36 +0200

    Update WPL Generator.bat

--- WPL Generator.bat
+++ WPL Generator.bat
@@ -9,8 +9,8 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
-along with Foobar. If not, see <http://www.gnu.org/licenses/>.
-Copyright (c) Meï-Garino Jérémy
+along with WPL Generator. If not, see <http://www.gnu.org/licenses/>.
+Copyright (c) MeÃ¯-Garino JÃ©rÃ©my
 :EndComment
 @echo off
 Setlocal EnableDelayedExpansion
@@ -19,9 +19,9 @@ set "output=%PlayListName%.wpl"
 set "ext=.png"
 set cnt=0
 for %%A in (*%ext%) do set /a cnt+=1
-set "head=<?wpl version="1.0"?><smil><head><meta name="Generator" content="Jérémy Meï-Garino WPL Generator Tool"/><meta name="ItemCount" content="%cnt%"/><title>%PlayListName%</title></head><body><seq>"
+set "head=<?wpl version="1.0"?><smil><head><meta name="Generator" content="JÃ©rÃ©my MeÃ¯-Garino WPL Generator Tool"/><meta name="ItemCount" content="%cnt%"/><title>%PlayListName%</title></head><body><seq>"
 set "tail=</seq></body></smil>"
-@echo génération de la liste des fichiers...
+@echo gÃ©nÃ©ration de la liste des fichiers...
 echo !head!> %output%
 for %%f in (*%ext%) do (
 	set "s=<media src="%%~nf.png"/>"
